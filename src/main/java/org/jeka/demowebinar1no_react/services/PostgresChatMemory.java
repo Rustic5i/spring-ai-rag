f8ad686c015ccf07da553a17cda7b739894a93c4
Rustic5i <rus5i@mail.ru>
2025-09-28 23:36:31 +0500

    init

---
+++ src/main/java/org/jeka/demowebinar1no_react/services/PostgresChatMemory.java
@@ -0,0 +1,52 @@
+package org.jeka.demowebinar1no_react.services;
+
+import lombok.Builder;
+import org.jeka.demowebinar1no_react.model.Chat;
+import org.jeka.demowebinar1no_react.model.ChatEntry;
+import org.jeka.demowebinar1no_react.repo.ChatRepository;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Comparator;
+import java.util.List;
+
+@Builder
+public class PostgresChatMemory implements ChatMemory {
+
+    private ChatRepository chatMemoryRepository;
+
+    private int maxMessages;
+
+    @Override
+    public void add(String conversationId, List<Message> messages) {
+        Chat chat = chatMemoryRepository.findById(Long.valueOf(conversationId)).orElseThrow();
+        for (Message message : messages) {
+            chat.addChatEntry(ChatEntry.toChatEntry(message));
+        }
+        chatMemoryRepository.save(chat);
+
+    }
+
+
+    @Override
+    public List<Message> get(String conversationId) {
+        Chat chat = chatMemoryRepository.findById(Long.valueOf(conversationId)).orElseThrow();
+        int messageToSkip = Math.max(0, chat.getHistory().size() - maxMessages);
+        return chat.getHistory().stream()
+//                .sorted(Comparator.comparing(ChatEntry::getCreatedAt)) //Закоментировали так как сортериуем на уровне entity @OrderBy("createdAt ASC")
+                .skip(messageToSkip)
+                .map(ChatEntry::toMessage)
+                .limit(maxMessages)
+                .toList();
+
+    }
+
+    @Override
+    public void clear(String conversationId) {
+        //not implemented
+    }
+}
